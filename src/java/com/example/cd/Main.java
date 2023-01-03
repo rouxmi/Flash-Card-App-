98be186c7f0028768e34ec68e0d070f477ef9f69
Boucher <lucie.boucher@telecomnancy.eu>
2023-01-03 11:48:23 +0100

    Gestion Controleur

--- src/java/com/example/cd/Main.java
+++ src/java/com/example/cd/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-       ;
+        MainStage = stage;
         MainStage.setScene(Mainscene);
         stage.show();
     }
