22045776a1651759d16f5b94fa3a8660a86208f8
Montignier <lola.montignier@telecomnancy.eu>
2023-01-03 11:27:47 +0100

    Creation Controleurs

---
+++ src/java/com/example/cd/controleurs/EntrainementControleur.java
@@ -0,0 +1,19 @@
+package com.example.cd.controleurs;
+
+import com.example.cd.Observateur;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EntrainementControleur implements Observateur, Initializable {
+    @Override
+    public void reagir() {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
