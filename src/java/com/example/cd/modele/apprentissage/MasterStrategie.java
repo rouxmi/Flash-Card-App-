fec0cda0b396888893760a13be61a458e5700e3e
Boucher <lucie.boucher@telecomnancy.eu>
2023-01-04 17:36:45 +0100

    MasterStrategie

--- src/java/com/example/cd/modele/apprentissage/MasterStrategie.java
+++ src/java/com/example/cd/modele/apprentissage/MasterStrategie.java
@@ -1,4 +1,46 @@
 package com.example.cd.modele.apprentissage;
 
-public class MasterStrategie {
+import com.example.cd.modele.Carte;
+import com.example.cd.modele.PaquetDeCartes;
+
+public class MasterStrategie implements ApprentissageStrategie{
+
+    @Override
+    public String toString() {
+        return "Master";
+    }
+
+    @Override
+    public Carte getCarte(PaquetDeCartes paquet, int index) {
+        double random = Math.random();
+        Carte c = paquet.getCarte((int)(Math.random()*paquet.taillePaquet()));
+        if(c.getStatsCarte().ValidableAujourdhui()){
+            return c;
+        }
+        switch (c.getStatsCarte().getEtatCarte()) {
+            case NonVue, ARevoir -> {
+                if (random < 0.75) {
+                    return c;
+                }
+            }
+            case DebutApprentissage -> {
+                if (random < 0.30) {
+                    return c;
+                }
+            }
+            case FinApprentissage -> {
+                if (random < 0.15 ) {
+                    return c;
+                }
+            }
+            case AcquiseParfaite -> {
+                if (random < 0.1) {
+                    return c;
+                }
+            }
+        }
+        return this.getCarte(paquet,index);
+    }
+
+
 }
