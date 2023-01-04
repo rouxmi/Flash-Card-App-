66e3208378040ac4354b6233df2b0b2513b1dda3
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2023-01-04 11:50:08 +0100

    [+] Enum stats

--- src/java/com/example/cd/statistiques/StatsCarte.java
+++ src/java/com/example/cd/statistiques/StatsCarte.java
@@ -1,4 +1,16 @@
 package com.example.cd.statistiques;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class StatsCarte {
+    private ArrayList<String> DateReussite;
+    private ArrayList<String> DateEchec;
+    private EtatCarte etatCarte;
+
+    public StatsCarte() {
+        DateReussite = new ArrayList<String>();
+        DateEchec = new ArrayList<String>();
+        etatCarte = EtatCarte.NonVue;
+    }
 }
