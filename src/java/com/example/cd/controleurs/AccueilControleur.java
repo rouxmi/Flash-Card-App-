5682136ea373f8bdc5cc609d09187059d98dfd34
Bourdais Rémi <remi.bourdais@telecomnancy.eu>
2023-01-06 13:54:09 +0100

    [+] Partage en ligne pour tous

--- src/java/com/example/cd/controleurs/AccueilControleur.java
+++ src/java/com/example/cd/controleurs/AccueilControleur.java
@@ -180,9 +180,31 @@ public class AccueilControleur extends SujetObserve implements Initializable, Ob
     }
     @FXML
     public void Partager(){
+        String url = "https://drive.google.com/drive/folders/1lVTIiVpMvUISgtlbWErNU_TKBQSxMgqF?usp=sharing";
+        String os = System.getProperty("os.name").toLowerCase();
+
         try {
-            new ProcessBuilder("x-www-browser", "https://drive.google.com/drive/folders/1lVTIiVpMvUISgtlbWErNU_TKBQSxMgqF?usp=sharing").start();
-        } catch (IOException e) {
+            if (os.contains("win")) {
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (os.contains("mac")) {
+                Runtime.getRuntime().exec("open " + url);
+            } else {
+                // Assuming a Unix-like system
+                String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+
+                String browser = null;
+                for (int count = 0; count < browsers.length && browser == null; count++) {
+                    if (Runtime.getRuntime().exec(new String[] {"which", browsers[count]}).waitFor() == 0) {
+                        browser = browsers[count];
+                    }
+                }
+                if (browser == null) {
+                    throw new Exception("Could not find web browser");
+                } else {
+                    Runtime.getRuntime().exec(new String[] {browser, url});
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
