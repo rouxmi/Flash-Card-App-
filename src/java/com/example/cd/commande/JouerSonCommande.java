1c29aa0b25ef694bb0258a88e4fb86016dea3f2f
Montignier <lola.montignier@telecomnancy.eu>
2023-01-06 17:20:20 +0100

    push

--- src/java/com/example/cd/commande/JouerSonCommande.java
+++ src/java/com/example/cd/commande/JouerSonCommande.java
@@ -1,5 +1,6 @@
 package com.example.cd.commande;
 
+import com.example.cd.Main;
 import com.example.cd.controleurs.GlobalControleur;
 import com.example.cd.modele.Carte;
 import javafx.scene.media.AudioClip;
@@ -19,7 +20,8 @@ public class JouerSonCommande extends Commande {
     public void execute() throws Exception {
         AudioClip player;
         if (coteCarte.equals("question")) {
-            player = new AudioClip(getClass().getResource(carte.getAudioQuestion()).toExternalForm());
+            System.out.println(carte.getAudioQuestion());
+            player = new AudioClip(Main.class.getResource(carte.getAudioQuestion()).toExternalForm());
         } else {
             player = new AudioClip(getClass().getResource(carte.getAudioReponse()).toExternalForm());
         }
